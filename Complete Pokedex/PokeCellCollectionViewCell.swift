738b96e75d0a90f44c301627e91bde8bd247936e
Danny Stover <30733045+MasterDonut@users.noreply.github.com>
2017-08-07 00:22:09 -0400

    before implementing data from the PokeAPI

---
+++ Complete Pokedex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  Complete Pokedex
+//
+//  Created by Danny Stover on 8/6/17.
+//  Copyright © 2017 Danny Stover. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    
+}
